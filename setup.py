a0afa001882cada86a46a7b047fcdbfea3d1c6fb
Diego Garay-Ruiz <dgaray@iciq.es>
2024-10-22 14:26:10 +0200

    Added find_packages to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 setup(name='jsmol_to_bokeh',
       version='0.5',
       author="Diego Garay-Ruiz, Leopold Talirz",
       description="Bokeh extension for JSMol, adapted to support precompilation",
       install_requires="bokeh>=2.3.2<3.0.0",
-      url="https://github.com/dgarayr/jsmol_to_bokeh"
+      url="https://github.com/dgarayr/jsmol_to_bokeh",
+      packages=find_packages()
 )
