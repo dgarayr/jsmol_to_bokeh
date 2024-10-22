7e05c48f2cbe632fc8877634e31fe2000306a9f2
Diego Garay-Ruiz <dgaray@iciq.es>
2024-10-22 14:06:58 +0200

    Added install file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='jsmol_to_bokeh',
+      version='0.5',
+      author="Diego Garay-Ruiz, Leopold Talirz",
+      description="Bokeh extension for JSMol, adapted to support precompilation",
+      install_requires="bokeh>=2.3.2<3.0.0",
+      url="https://github.com/dgarayr/jsmol_to_bokeh"
+)
