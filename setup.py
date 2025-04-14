3645fbdc4b3b9ece3d6513346c4ecaa38ecbeb97
Diego Garay Ruiz <diegogarayruiz@gmail.com>
2025-04-14 11:56:15 +0200

    Fixed versioning in setup.py for later py versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='jsmol_to_bokeh',
       version='0.5',
       author="Diego Garay-Ruiz, Leopold Talirz",
       description="Bokeh extension for JSMol, adapted to support precompilation",
-      install_requires="bokeh>=2.3.2<3.0.0",
+      install_requires="bokeh>=2.3.2,<3.0.0",
       url="https://github.com/dgarayr/jsmol_to_bokeh",
       py_modules=['jsmol_to_bokeh']
 )
